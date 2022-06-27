2c903dbdd7429e983406d4f2dd1fa39bf72c4e49
byFloriiian <florian.eggetsbergerfefe@gmail.com>
2022-06-27 22:03:18 +0200

    MOSU-04: Added Service + Controller

---
+++ MovieSuggest/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieSuggest.Models;
+using MovieSuggest.Services;
+
+namespace MovieSuggest.Controllers
+{
+    public class MovieController : Controller
+    {
+        private MovieApiService movieApiService;
+
+        public MovieController(MovieApiService movieApiService)
+        {
+            this.movieApiService = movieApiService;
+        }
+        
+        [HttpGet("movies/{searchterm}")]
+        public async Task<List<ApiMovie>> GetMoviesAsync(string searchterm)
+        {
+            return await movieApiService.APIGetMoviesBySearchterm(searchterm);
+        }
+    }
+}
---
+++ MovieSuggest/Services/MovieApiService.cs
@@ -0,0 +1,33 @@
+﻿using MovieSuggest.Models;
+using Newtonsoft.Json;
+
+namespace MovieSuggest.Services
+{
+    public class MovieApiService
+    {
+        private static string _baseUrl = @"https://api.themoviedb.org/3/";
+        private string _apiKey = "?api_key=51f3ddcab9cd18864f234b69a16ff346";
+        private string _sessionId = "&guest_session_id=c00868a67576bfdad555e73754326c49";
+
+        public async Task<List<ApiMovie>> APIGetMoviesBySearchterm(string searchterm)
+        {
+            string url = _baseUrl + "search/movie" + _apiKey + _sessionId + $"&query={searchterm}";
+            HttpClient client = new HttpClient();
+            var data = new List<ApiMovie>();
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                Data responseBody = await response.Content.ReadFromJsonAsync<Data>();
+                Console.WriteLine(responseBody);
+                data = responseBody.results;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return data;
+        }
+    }
+}
